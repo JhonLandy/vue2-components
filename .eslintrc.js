1672d0a61c3d54be8f9367493560095162cf33e6
huaweiserver <709364178@qq.com>
2022-11-25 09:10:31 +0800

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: {
+      ts: true,
+      jsx: true,
+      experimentalObjectRestSpread: true,
+    },
+  },
+  env: {
+    es6: true,
+    node: true,
+    browser: true,
+    commonjs: true,
+  },
+  extends: [
+    "plugin:vue/vue3-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  rules: {
+    "no-var": "error",
+    "no-empty": "warn",
+    "vue/multi-word-component-names": "off",
+    "no-def": "error"
+  },
+};
